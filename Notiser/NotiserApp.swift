446d307539f31607e27616fd723e16cb9c904ba9
Bill Mårtensson <bill@magictechnology.se>
2020-11-26 09:13:43 +0100

    Initial Commit

---
+++ Notiser/NotiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiserApp.swift
+//  Notiser
+//
+//  Created by Bill Martensson on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct NotiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
